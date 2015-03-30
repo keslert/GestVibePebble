c27ff7ccc754df8b5d1775cbda01708de89255b4
Kesler <keslert@gmail.com>
2015-03-30 16:14:25 -0700

    The updated code

--- src/vibrations.c
+++ src/vibrations.c
@@ -1,7 +1,8 @@
 #include <pebble.h>
 #include "vibrations.h"
 
-static VibePattern patterns[10]; 
+static VibePattern patterns[10];
+static VibePattern transitions[2];
 
 static const uint32_t p0[] = { 100 };
 static const uint32_t p1[] = { 100, 150, 100 };
@@ -13,6 +14,10 @@ static const uint32_t p6[] = { 500, 150, 100, 150, 100 };
 static const uint32_t p7[] = { 500, 150, 100, 150, 100, 150, 100 };
 static const uint32_t p8[] = { 500, 150, 100, 150, 100, 150, 100, 150, 100 };
 static const uint32_t p9[] = { 500, 150, 500 };
+
+// Transition between two different vibrations
+static const uint32_t t0[] = { 350, 100, 50, 100, 50, 100, 50, 100, 350 };
+static const uint32_t t1[] = { 1500 };
 
 void init_vibrations() {
   patterns[0].durations = p0;
@@ -35,9 +40,42 @@ void init_vibrations() {
   patterns[8].num_segments = ARRAY_LENGTH(p8);
   patterns[9].durations = p9;
   patterns[9].num_segments = ARRAY_LENGTH(p9);
+  
+  transitions[0].durations = t0;
+  transitions[0].num_segments = ARRAY_LENGTH(t0);
+  transitions[1].durations = t1;
+  transitions[1].num_segments = ARRAY_LENGTH(t1);
 }
 
 void play_pattern(int pattern)
 {
-  vibes_enqueue_custom_pattern(patterns[pattern - 1]);
+  if(pattern <= 10) {
+    vibes_enqueue_custom_pattern(patterns[pattern - 1]);
+  } else {
+    int p1 = pattern / 100;
+    int p2 = pattern % 100;
+    
+    VibePattern vp1 = patterns[p1 - 1];
+    VibePattern vp2 = patterns[p2 - 1];
+    VibePattern t = transitions[1];
+    
+    uint32_t* combined = malloc((vp1.num_segments + vp2.num_segments + t.num_segments) * sizeof(uint32_t)); // array to hold the result
+
+    memcpy(combined,                                      vp1.durations, vp1.num_segments * sizeof(uint32_t)); 
+    memcpy(combined + vp1.num_segments,                   t.durations,   t.num_segments * sizeof(uint32_t)); 
+    memcpy(combined + vp1.num_segments + t.num_segments,  vp2.durations, vp2.num_segments * sizeof(uint32_t));
+    
+    VibePattern p = {
+      .durations = combined,
+      .num_segments = vp1.num_segments + vp2.num_segments + t.num_segments
+    };
+    
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Combining resulted in: %d", (int)(vp1.num_segments + vp2.num_segments + t.num_segments)); 
+    
+    vibes_enqueue_custom_pattern(p);
+    
+//     vibes_enqueue_custom_pattern(patterns[p1  - 1]);
+//     vibes_enqueue_custom_pattern(transitions[0]);
+//     vibes_enqueue_custom_pattern(patterns[p2  - 1]);
+  }
 }
